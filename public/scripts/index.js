f80de9ca459db96904d3abe7889899e875e38522
Mattazz <matthewazada101@gmail.com>
2022-12-26 14:31:05 +0800

    added animated stylings

---
+++ public/scripts/index.js
@@ -0,0 +1,15 @@
+anime({
+    targets: '.animate-box',
+    translateX: [100, -100],
+    loop: true,
+    direction: 'alternate',
+    duration: 2000,
+    easing: 'cubicBezier(.5, 0.5, .1, .3)'
+})
+
+anime({
+    targets: '.animate-circle',
+    scale: 0,
+    loop: true,
+    direction: 'alternate'
+})
